a894cb3865d4878f941dbe91e5b85e3a089b5722
chengq <157745085@qq.com>
2019-06-13 15:04:03 +0800

    加入捕获udp数据包

--- src/main/java/cn/ninetailfox/App.java
+++ src/main/java/cn/ninetailfox/App.java
@@ -6,6 +6,7 @@ import com.alibaba.fastjson.JSONObject;
 import jpcap.JpcapCaptor;
 import jpcap.NetworkInterface;
 import jpcap.packet.Packet;
+import jpcap.packet.TCPPacket;
 import jpcap.packet.UDPPacket;
 
 import java.io.IOException;
@@ -31,73 +32,124 @@ public class App
                 new LinkedBlockingDeque<>());
 
         for (NetworkInterface inter : interfaces) {
-            pool.execute(() -> {
-                try {
-                    JpcapCaptor cap = JpcapCaptor.openDevice(inter, 65535, true, 50);
-                    cap.setFilter("dst 192.168.1.4", true);
-
-                    Map<String, JSONObject> portMap = new ConcurrentHashMap<>();
-                    Map<String, Boolean> portStatus = new ConcurrentHashMap<>();
-
-                    while (true) {
-                        Packet packet = cap.getPacket();
-                        if (packet instanceof UDPPacket) {
-                            UDPPacket udpPacket = (UDPPacket) packet;
-                            String port = String.valueOf(udpPacket.dst_port);
-                            System.out.println("udp to: " + port);
-
-                            if (portStatus.get(port) != null) {
-                                if (portStatus.get(port)) {
-                                    System.out.println("is rtp");
-                                    // todo 已经验证是rtp流
-                                } else {
-                                    System.out.println("not rtp");
-                                }
-                            } else {
-                                System.out.println("checking");
-                                JSONObject result = CheckPacketUtil.getRtpInfo(udpPacket.data);
-                                if (result.getBoolean("match")) {
-                                    if (portMap.get(port) == null) {
-                                        JSONObject info = new JSONObject();
-                                        info.put("payloadType", result.getInteger("payloadType"));
-                                        info.put("ssrc", result.getString("ssrc"));
-                                        info.put("lastSequenceNumber", result.getLongValue("sequenceNumber"));
-                                        info.put("lastTimestamp", result.getLongValue("timestamp"));
-                                        info.put("remainCheckTime", 4);
-                                        portMap.put(port, info);
-                                        continue;
-                                    }
-                                    JSONObject portInfo = portMap.get(port);
-                                    if (portInfo.getIntValue("remainCheckTime") <= 0) {
-                                        portMap.remove(port);
-                                        portStatus.put(port, true);
-                                        continue;
-                                    }
-                                    long deltaSequenceNumber = result.getLong("sequenceNumber") - portInfo.getLong("lastSequenceNumber");
-                                    long deltaTimestamp = result.getLong("timestamp") - portInfo.getLong("lastTimestamp");
-                                    boolean sequenceNumberAndTimestampMatch = (deltaSequenceNumber > 0 && deltaTimestamp > 0) || (deltaSequenceNumber < 0 && deltaTimestamp < 0);
-                                    if (portInfo.getIntValue("payloadType") != (result.getIntValue("payloadType"))
-                                            || !portInfo.getString("ssrc").equals(result.getString("ssrc"))
-                                            || !sequenceNumberAndTimestampMatch) {
-                                        portMap.remove(port);
-                                        portStatus.put(port, false);
-                                        continue;
-                                    }
-                                    portInfo.put("lastSequenceNumber", result.getLong("sequenceNumber"));
-                                    portInfo.put("lastTimestamp", result.getLong("timestamp"));
-                                    portInfo.put("remainCheckTime", portInfo.getIntValue("remainCheckTime") - 1);
-                                } else {
-                                    portMap.remove(port);
-                                    portStatus.put(port, false);
-                                }
-                            }
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
+//            pool.execute(new RecognizeRtpRunnable(inter));
+            pool.execute(new RecognizeUdpRunnable(inter));
         }
 
     }
+
+    /**
+     * 识别rtp
+     * @author wh_chengq
+     * @date 2019/6/13
+     */
+    private static class RecognizeRtpRunnable implements Runnable {
+
+        private NetworkInterface inter;
+
+        public RecognizeRtpRunnable(NetworkInterface networkInterface) {
+            this.inter = networkInterface;
+        }
+
+        @Override
+        public void run() {
+            try {
+                JpcapCaptor cap = JpcapCaptor.openDevice(inter, 65535, true, 50);
+                cap.setFilter("dst 192.168.1.4", true);
+
+                Map<String, JSONObject> portMap = new ConcurrentHashMap<>();
+                Map<String, Boolean> portStatus = new ConcurrentHashMap<>();
+
+                while (true) {
+                    Packet packet = cap.getPacket();
+                    if (packet instanceof UDPPacket) {
+                        UDPPacket udpPacket = (UDPPacket) packet;
+                        String port = String.valueOf(udpPacket.dst_port);
+                        System.out.println("udp to: " + port);
+
+                        if (portStatus.get(port) != null) {
+                            if (portStatus.get(port)) {
+                                System.out.println("is rtp");
+                                // todo 已经验证是rtp流
+                            } else {
+                                System.out.println("not rtp");
+                            }
+                        } else {
+                            System.out.println("checking");
+                            JSONObject result = CheckPacketUtil.getRtpInfo(udpPacket.data);
+                            if (result.getBoolean("match")) {
+                                if (portMap.get(port) == null) {
+                                    JSONObject info = new JSONObject();
+                                    info.put("payloadType", result.getInteger("payloadType"));
+                                    info.put("ssrc", result.getString("ssrc"));
+                                    info.put("lastSequenceNumber", result.getLongValue("sequenceNumber"));
+                                    info.put("lastTimestamp", result.getLongValue("timestamp"));
+                                    info.put("remainCheckTime", 4);
+                                    portMap.put(port, info);
+                                    continue;
+                                }
+                                JSONObject portInfo = portMap.get(port);
+                                if (portInfo.getIntValue("remainCheckTime") <= 0) {
+                                    portMap.remove(port);
+                                    portStatus.put(port, true);
+                                    continue;
+                                }
+                                long deltaSequenceNumber = result.getLong("sequenceNumber") - portInfo.getLong("lastSequenceNumber");
+                                long deltaTimestamp = result.getLong("timestamp") - portInfo.getLong("lastTimestamp");
+                                boolean sequenceNumberAndTimestampMatch = (deltaSequenceNumber > 0 && deltaTimestamp > 0) || (deltaSequenceNumber < 0 && deltaTimestamp < 0);
+                                if (portInfo.getIntValue("payloadType") != (result.getIntValue("payloadType"))
+                                        || !portInfo.getString("ssrc").equals(result.getString("ssrc"))
+                                        || !sequenceNumberAndTimestampMatch) {
+                                    portMap.remove(port);
+                                    portStatus.put(port, false);
+                                    continue;
+                                }
+                                portInfo.put("lastSequenceNumber", result.getLong("sequenceNumber"));
+                                portInfo.put("lastTimestamp", result.getLong("timestamp"));
+                                portInfo.put("remainCheckTime", portInfo.getIntValue("remainCheckTime") - 1);
+                            } else {
+                                portMap.remove(port);
+                                portStatus.put(port, false);
+                            }
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 识别udp
+     * @author wh_chengq
+     * @date 2019/6/13
+     */
+    private static class RecognizeUdpRunnable implements Runnable {
+
+        private NetworkInterface inter;
+
+        public RecognizeUdpRunnable(NetworkInterface networkInterface) {
+            this.inter = networkInterface;
+        }
+
+        @Override
+        public void run() {
+            JpcapCaptor cap = null;
+            try {
+                cap = JpcapCaptor.openDevice(inter, 65535, true, 50);
+                cap.setFilter("udp and port 443", true);
+
+                while (true) {
+                    Packet packet = cap.getPacket();
+                    if (packet instanceof TCPPacket) {
+                        System.out.println(((TCPPacket) packet).src_port + "->" + ((TCPPacket) packet).dst_port);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
